7e8986e63e7038ebfe22a4b35ba71cecb13349ba
nkondrat <nkondrat@softserveinc.com>
2024-08-18 20:25:16 +0300

    add task4

--- src/main/java/jom/com/softserve/s6/task4/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task4/MyUtils.java
@@ -1,6 +1,7 @@
 package jom.com.softserve.s6.task4;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -8,6 +9,25 @@ import java.util.Locale;
 
 public class MyUtils {
 
-    //Write your code here
+    public static String readFile(String filename) {
+        StringBuilder readableString = new StringBuilder();
 
+        try (FileReader fileReader = new FileReader(filename)) {
+            StringBuilder binaryString = new StringBuilder();
+            int c = 0;
+
+            while ((c = fileReader.read()) != -1) {
+                binaryString.append((char) c);
+
+                if (binaryString.length() == 7) {
+                    int charCode = Integer.parseInt(binaryString.toString(), 2);
+                    readableString.append((char) charCode);
+                    binaryString.setLength(0);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return readableString.toString();
+    }
 }
