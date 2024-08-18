313d7d63c4034f23897c0153c5bf2de648d12978
nkondrat <nkondrat@softserveinc.com>
2024-08-18 16:02:23 +0300

    add task3

--- src/main/java/jom/com/softserve/s6/task3/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task3/MyUtils.java
@@ -1,11 +1,20 @@
 package jom.com.softserve.s6.task3;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class MyUtils {
 
-    //Write your code here
+    public static void writeFile(String filename, String text)  {
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            for (char c : text.toCharArray()) {
+                fos.write(String.format("%7s", Integer.toBinaryString(c)).replace(' ', '0').getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
