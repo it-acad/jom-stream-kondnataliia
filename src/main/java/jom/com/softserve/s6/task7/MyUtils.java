9497b49834ee27bd336904c00d8f5a8852943078
nkondrat <nkondrat@softserveinc.com>
2024-08-20 21:27:25 +0300

    add task7

--- src/main/java/jom/com/softserve/s6/task7/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task7/MyUtils.java
@@ -1,6 +1,6 @@
 package jom.com.softserve.s6.task7;
 
-import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,6 +8,13 @@ import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
-
+    public Stream<Integer> duplicateElements(Stream<Integer> stream) {
+        //Set<Integer> uniqueValues = new HashSet<>();
+        return stream.filter(Objects::nonNull)
+                     .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                     .entrySet().stream()
+                                .filter(entry -> entry.getValue() > 1)
+                                .map(Map.Entry::getKey)
+                                .sorted();
+    }
 }
